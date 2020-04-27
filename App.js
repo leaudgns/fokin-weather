37b4573ffeebbdaa8f895c943e8a6f9c74c53ee7
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-27 17:22:38 +0900

    ReactNative 1.1

--- App.js
+++ App.js
@@ -1,34 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>Hello!</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>Nice to meet you</Text>
-      </View>
-    </View>
-  );
+  return <Loading></Loading>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  text: {
-    color: "black",
-    width: "100%",
-    textAlign: "center",
-  },
-  yellowView: {
-    flex: 2,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 4,
-    backgroundColor: "blue",
-  },
-});
