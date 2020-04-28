2c94444c5b162768956c2331ba6d938b779ff6e9
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-28 16:40:45 +0900

    ReactNative 2.3

--- Loading.js
+++ Loading.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function Loading() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#FDF6AA" />
       <Text style={styles.text}>Getting the smart weather</Text>
     </View>
   );
